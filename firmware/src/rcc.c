4e8a224a9e29592b5babbeb2259035013b8759fa
miesho <marcin.ryzewskii@gmail.com>
2025-04-23 15:22:53 +0200

    remove random coment

--- firmware/src/rcc.c
+++ firmware/src/rcc.c
@@ -6,20 +6,14 @@
 
 void rcc_init() {
     // CONFIG SYSTEM CLOCK TO 80MHz
-    //
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Configure the main internal regulator output voltage */
     if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
     {
         _panic();
     }
 
-    /** 
-      * Initializes the RCC Oscillators according to the specified parameters
-      * in the RCC_OscInitTypeDef structure.
-      */
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
     RCC_OscInitStruct.MSIState = RCC_MSI_ON;
     RCC_OscInitStruct.MSICalibrationValue = 0;
@@ -36,9 +30,6 @@ void rcc_init() {
         _panic();
     }
 
-    /*
-     * Initializes the CPU, AHB and APB buses clocks
-    */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
