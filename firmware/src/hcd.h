93a7874deeefb804ca47ca28e94ac2d477574050
miesho <marcin.ryzewskii@gmail.com>
2025-04-23 15:20:07 +0200

    Init hcd driver

--- firmware/src/hcd.h
+++ firmware/src/hcd.h
@@ -3,7 +3,7 @@
 
 #include "stm32l4xx_hal_hcd.h"
 
-HCD_HandleTypeDef *usb_hard_take();
-void usb_hard_init();
+void usb_otg_fs_hcd_init(void);
+HCD_HandleTypeDef *usb_org_fs_hcd_take(void);
 
 #endif
