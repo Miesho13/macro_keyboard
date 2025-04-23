a18575466f607deabc27fe56cd1c5f9f316d3d53
miesho <marcin.ryzewskii@gmail.com>
2025-04-23 15:23:19 +0200

    Init in hcd in main

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -2,6 +2,7 @@
 #include "stm32l476xx.h"
 #include "stm32l4xx_hal.h"
 #include "rcc.h"
+#include "hard_usb.h"
 
 void SysTick_Handler(void) {
     HAL_IncTick();
@@ -10,23 +11,8 @@ void SysTick_Handler(void) {
 int main() {
     HAL_Init();
     rcc_init();
+    usb_otg_fs_hcd_init();
 
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
-
-    GPIO_InitTypeDef GPIO_Init = {
-        .Pin = GPIO_PIN_5,
-        .Mode = GPIO_MODE_OUTPUT_PP,
-        .Pull = GPIO_PULLDOWN,
-        .Speed = GPIO_SPEED_FREQ_LOW,
-        .Alternate = 0,
-    };
-
-    HAL_GPIO_Init(GPIOA, &GPIO_Init);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
-
-    while (1) { 
-        HAL_Delay(1000);
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-    }
+    while (1) { }
 }
 
