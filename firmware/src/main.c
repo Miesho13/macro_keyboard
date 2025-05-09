abd85581cc01be366a40516f774090a7e7a4dd67
miesho <marcin.ryzewskii@gmail.com>
2025-05-09 23:00:41 +0200

    Update main function

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -2,7 +2,7 @@
 #include "stm32l476xx.h"
 #include "stm32l4xx_hal.h"
 #include "rcc.h"
-#include "hcd.h"
+#include "tusb.h"
 
 void SysTick_Handler(void) {
     HAL_IncTick();
@@ -13,6 +13,10 @@ int main() {
     rcc_init();
     // usb_otg_fs_hcd_init();
 
-    while (1) { }
+    tuh_init(0);
+
+    while (1) { 
+        tuh_task();
+    }
 }
 
