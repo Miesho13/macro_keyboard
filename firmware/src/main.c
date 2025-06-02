bde2faae1edb821a6ad96c6f4da490f2b7185c05
miesho <marcin.ryzewskii@gmail.com>
2025-06-02 19:49:09 +0200

    Update main.c

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -2,7 +2,50 @@
 #include "stm32l476xx.h"
 #include "stm32l4xx_hal.h"
 #include "rcc.h"
-#include "tusb.h"
+#include "usb_host.h"
+#include "usbh_def.h"
+#include "usbh_hid.h"
+#include "usbh_hid_keybd.h"
+
+extern USBH_HandleTypeDef hUsbHostFS;
+extern ApplicationTypeDef Appli_state;
+
+
+void HID_handle() {
+    static uint8_t last_key = 0;
+
+    if (USBH_HID_GetDeviceType(&hUsbHostFS) != HID_KEYBOARD) {
+        return;
+    }
+
+    HID_KEYBD_Info_TypeDef *k_pinfo = USBH_HID_GetKeybdInfo(&hUsbHostFS);
+    if (k_pinfo == NULL) {
+        return;
+    }
+
+    uint8_t ascii = USBH_HID_GetASCIICode(k_pinfo);
+    if (ascii != 0 && ascii != last_key) {
+        last_key = ascii;
+
+        switch (ascii) {
+            case '0' ... '9': {
+                break;
+            } 
+
+            case '.': {
+                break;
+            }
+
+            case '\n': {
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+}
 
 void SysTick_Handler(void) {
     HAL_IncTick();
@@ -12,15 +55,12 @@ int main() {
     HAL_Init();
     rcc_init();
 
-    tusb_rhport_init_t host_init = {
-         .role = TUSB_ROLE_HOST,
-         .speed = TUSB_SPEED_AUTO
-    };
-
-    tusb_init(0, &host_init);
-
+    MX_USB_HOST_Init();
     while (1) { 
-        tuh_task();
+        MX_USB_HOST_Process();
+        if (Appli_state == APPLICATION_READY) {
+            HID_handle();
+        }
     }
 }
 
